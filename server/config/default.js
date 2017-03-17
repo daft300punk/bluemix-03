96388f34c207041228bc654b1458a3877761b0d6
Abhilash Singh <daft300punk@gmail.com>
2017-03-17 15:14:21 +0530

    chore: deleted redundant files

--- server/config/default.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-commonjs: 0, no-magic-numbers: 0 */
-
-module.exports = {
-  PORT: process.env.PORT || 3200,
-  VERBOSE_LOGGING: true,
-};
--- server/config/test.js
+++
@@ -1,5 +0,0 @@
-/* eslint import/no-commonjs: 0 */
-
-module.exports = {
-  VERBOSE_LOGGING: false,
-};
