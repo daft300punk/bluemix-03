ca0c889dd8c7a671c2641a13c23932901a4a7fc2
Abhilash Singh <daft300punk@gmail.com>
2017-03-18 23:37:15 +0530

    feat: Added redux actions

--- common/actions/index.js
+++ common/actions/index.js
@@ -1,3 +1,24 @@
-export const testAction = () => ({
-  type: 'world',
+import * as ActionTypes from '../constants';
+import {fetchInsightApi} from '../apis';
+
+const fetchInsightRequest = (handle) => ({
+  type: ActionTypes.FETCH_INSIGHT_REQUEST,
+  handle: handle
 });
+
+const fetchInsightSuccess = (result) => ({
+  type: ActionTypes.FETCH_INSIGHT_SUCCESS,
+  insight: result,
+});
+
+const fetchInsightFailure = (result) => ({
+  type: ActionTypes.FETCH_INSIGHT_FAILURE,
+  error: error,
+});
+
+const fetchInsight = (handle) => (dispatch) => {
+  dispatch(fetchInsightRequest(handle));
+  fetchInsightApi(handle)
+  .then(insight => dispatch(fetchInsightSuccess(insight)))
+  .catch(err => dispatch(fetchInsightFailure(err)));
+}
