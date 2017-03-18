c417547b3f3d070eb18bfc1b456a37df8862ae4e
Abhilash Singh <daft300punk@gmail.com>
2017-03-18 23:36:02 +0530

    feat: Added client fetch insight from server api

---
+++ common/apis/index.js
@@ -0,0 +1,17 @@
+export const fetchInsightApi = (handle) => {
+  return new Promise(function(resolve, reject) {
+    const url = '/api/personality-insights/?handle=' + handle;
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        let personalityInsight = {
+          needs: res.needs.map((val) => ({ name: val.name, percentile: val.percentile })),
+          values: res.values.map((val) => ({ name: val.name, percentile: val.percentile })),
+          personality: res.values.map((val) => ({ name: val.name, percentile: val.percentile }))
+        };
+        console.log(personalityInsight);
+        resolve(personalityInsight);
+      })
+      .catch(err => reject(err));
+  });
+};
